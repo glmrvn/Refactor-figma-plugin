c195c958d3e82371efff7b78bc39e2d09346f108
glmrvn <glmrvn@gmail.com>
2020-12-05 12:19:05 +0300

    Added UI

--- code.js
+++ code.js
@@ -1,27 +1,36 @@
-const instances = figma.root.findAll(node => node.type === "INSTANCE" && node.parent.type === "PAGE")
-const frames = figma.root.findAll(node => node.type === "FRAME" && node.parent.type === "PAGE")
-const components = figma.root.findAll(node => node.type === "COMPONENT" && node.parent.type === "PAGE")
+figma.showUI(__html__, { width: 240, height: 120 })
 
-var allNames = [...frames, ...instances, ...components].map(it => it.name)
-var compareNames = ["test", "name", "zoom"]
+figma.ui.onmessage = async (msg) => {
+    if (msg.type === 'codeNames') {
+        console.log(msg.codeNames)
+        var compareNames = msg.codeNames
 
-let difference = allNames.filter(x => !compareNames.includes(x));
-var problemObjects = [];
+        const instances = figma.root.findAll(node => node.type === "INSTANCE" && node.parent.type === "PAGE")
+        const frames = figma.root.findAll(node => node.type === "FRAME" && node.parent.type === "PAGE")
+        const components = figma.root.findAll(node => node.type === "COMPONENT" && node.parent.type === "PAGE")
 
-for (let name in difference) {
-    let frame = difference[name];
-    let find = figma.root.findAll(node => node.name === frame);
-    problemObjects.push(find[0]);
-    continue;
-}
+        var allNames = [...frames, ...instances, ...components].map(it => it.name)
 
-// console.log(problemObjects)
-// console.log(difference)
+        let difference = allNames.filter(x => !compareNames.includes(x));
+        var problemObjects = [];
 
-if (problemObjects.length > 0) {
-    figma.currentPage.selection = problemObjects
-    figma.viewport.scrollAndZoomIntoView(problemObjects)
-    figma.closePlugin("🚨 Resources not used")
-} else {
-    figma.closePlugin()
+        for (let name in difference) {
+            let frame = difference[name];
+            let find = figma.root.findAll(node => node.name === frame);
+            problemObjects.push(find[0]);
+            continue;
+        }
+
+        // console.log(problemObjects)
+        // console.log(difference)
+        // ["test", "name", "zoom"]
+
+        if (problemObjects.length > 0) {
+            figma.currentPage.selection = problemObjects
+            figma.viewport.scrollAndZoomIntoView(problemObjects)
+            figma.closePlugin("🚨 Not used in code")
+        } else {
+            figma.closePlugin()
+        }
+    }
 }
