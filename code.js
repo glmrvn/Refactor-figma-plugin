9c79f532d9fadf5a4601e0f25e3a8ac1bf9b1179
Alex Dyakov <glmrvn@gmail.com>
2021-12-23 11:42:46 +0300

    Updated

--- code.js
+++ code.js
@@ -2,35 +2,50 @@ figma.showUI(__html__, { width: 240, height: 120 })
 
 figma.ui.onmessage = async (msg) => {
     if (msg.type === 'codeNames') {
-        console.log(msg.codeNames)
+        // console.log(msg.codeNames)
         var compareNames = msg.codeNames
 
-        const instances = figma.currentPage.findAll(node => node.type === "INSTANCE" && node.parent.type === "PAGE")
-        const frames = figma.currentPage.findAll(node => node.type === "FRAME" && node.parent.type === "PAGE")
-        // const components = figma.currentPage.findAll(node => node.type === "COMPONENT" && node.parent.type === "PAGE")
+        function parentsCount(node) {
+            let parentsCount = 0
+            let parent = node.parent
+            
+            while (parent != null) {
+                parentsCount++
+                parent = parent.parent
+            }
 
-        var allNames = [...frames, ...instances].map(it => it.name)
-
-        let difference = allNames.filter(x => !compareNames.includes(x));
-        var problemObjects = [];
-
-        for (let name in difference) {
-            let frame = difference[name];
-            let find = figma.root.findAll(node => node.name === frame);
-            problemObjects.push(find[0]);
-            continue;
+            return parentsCount
         }
 
-        // console.log(problemObjects)
-        // console.log(difference)
-        // ["test", "name", "zoom"]
+        function isContainerFrame(node) {
+            return node.type === "FRAME" && node.name.includes(":")
+        }
 
-        if (problemObjects.length > 0) {
-            figma.currentPage.selection = problemObjects
-            figma.viewport.scrollAndZoomIntoView(problemObjects)
-            figma.closePlugin(`🚨 Not used in code ${problemObjects.length}`, problemObjects)
+        function isValidNode(node) {
+            return node.type === "INSTANCE" || node.type === "COMPONENT" || node.type === "FRAME"
+        }
+
+        const allNodes = figma.currentPage.findAll(node => {
+            if (parentsCount(node) == 2) {
+                return isValidNode(node) && !isContainerFrame(node)
+            } else if (parentsCount(node) == 3) {
+                return isValidNode(node) && !isContainerFrame(node) && isContainerFrame(node.parent)
+            } else {
+                return false
+            }
+        })
+
+        // SELECT TO REMOVE
+        const namesToRemove = compareNames.split(",")
+        const nodesToRemove = allNodes.filter(node => namesToRemove.includes(node.name))
+        console.log(nodesToRemove.length)
+
+        if (nodesToRemove.length > 0) {
+            figma.currentPage.selection = nodesToRemove
+            figma.viewport.scrollAndZoomIntoView(nodesToRemove)
+            figma.closePlugin(`🚨 Not used in code ${nodesToRemove.length}`, nodesToRemove)
         } else {
-            figma.closePlugin(`👌 Used ${allNames.length} assets `, allNames)
+            figma.closePlugin("👌")
         }
     }
 }
